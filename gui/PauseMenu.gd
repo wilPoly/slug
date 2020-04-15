b1eec86e2effb19c38fdc4974eec51834631b095
wilPoly <misterwrinkle@gmail.com>
2020-04-15 15:37:16 +0200

    Add pause menu (fix)

--- gui/PauseMenu.gd
+++ gui/PauseMenu.gd
@@ -1,15 +1,6 @@
 extends Control
 
 
-<<<<<<< Updated upstream
-
-func _on_Continue_button_up() -> void:
-	get_tree().paused = false
-
-
-func _on_Retry_button_up() -> void:
-	get_tree().reload_current_scene()
-=======
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		var new_pause_state = not get_tree().paused
@@ -20,7 +11,6 @@ func _input(event: InputEvent) -> void:
 func _on_RestartLevel_button_up() -> void:
 	get_tree().reload_current_scene()
 	get_tree().paused = false
->>>>>>> Stashed changes
 
 
 func _on_Quit_button_up() -> void:
